caea8ed7514df67ce6099cd96c00de81f4a64c3e
Scott Evans <scottvevans@gmail.com>
2020-05-11 21:51:36 +0300

    initial check-in

---
+++ src/main/java/com/scottvevans/linkchecker/LinkcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.scottvevans.linkchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scottvevans/linkchecker/LinkcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottvevans.linkchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
